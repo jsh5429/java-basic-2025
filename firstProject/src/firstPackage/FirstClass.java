9b5b7ff75577bf5ba78d5256416bfe99ccfe935a
SH J <thdghk5429@gmail.com>
2025-01-08 17:03:53 +0900

    first commit

---
+++ firstProject/src/firstPackage/FirstClass.java
@@ -0,0 +1,12 @@
+package firstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("정송화");
+		//System.out.println("Java");
+		System.out.println(/*"안녕하세요."*/"안녕히가세요.");
+
+	}
+
+}
